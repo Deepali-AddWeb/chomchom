537d526934b4a0c4de0beae636c44353daaecbf8
dharmesh-addweb <dharmesh.addweb@gmail.com>
2016-02-19 19:58:25 +0530

    all scss file and css file

--- sites/all/themes/chomchom/js/custom.js
+++ sites/all/themes/chomchom/js/custom.js
@@ -6,3 +6,12 @@
   }
   };
 }(jQuery));
+
+(function ($) {
+  Drupal.behaviors.chomchom1 = {
+  attach: function (context, settings) {
+    jQuery('input[type="radio"]').wrap('<div class="radio-rc"></div>');
+    jQuery('.radio-rc').append('<span class="radio-rc-span"></span>');
+  }
+  };
+}(jQuery));
