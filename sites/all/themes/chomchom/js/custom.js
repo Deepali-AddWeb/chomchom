8779be4849d6f7cc64496b4eb4b51197892e4e70
dharmesh-addweb <dharmesh.addweb@gmail.com>
2016-02-22 18:18:08 +0530

    commerce page changes and business directory listing page css

--- sites/all/themes/chomchom/js/custom.js
+++ sites/all/themes/chomchom/js/custom.js
@@ -1,17 +1,17 @@
 (function ($) {
   Drupal.behaviors.chomchom = {
-  attach: function (context, settings) {
-    jQuery('input[type="checkbox"]').wrap('<div class="input-rc"></div>');
-    jQuery('.input-rc').append('<span class="input-rc-span"></span>');
-  }
+    attach: function (context, settings) {
+      jQuery('input[type="checkbox"]').wrap('<div class="input-rc"></div>');
+      jQuery('.input-rc').append('<span class="input-rc-span"></span>');
+    }
   };
 }(jQuery));
 
 (function ($) {
   Drupal.behaviors.chomchom1 = {
-  attach: function (context, settings) {
-    jQuery('input[type="radio"]').wrap('<div class="radio-rc"></div>');
-    jQuery('.radio-rc').append('<span class="radio-rc-span"></span>');
-  }
+    attach: function (context, settings) {
+      jQuery('input[type="radio"]').wrap('<div class="radio-rc"></div>');
+      jQuery('.radio-rc').append('<span class="radio-rc-span"></span>');
+    }
   };
 }(jQuery));
