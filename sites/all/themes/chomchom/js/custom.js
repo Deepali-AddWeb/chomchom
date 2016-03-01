f0753e91039c060d15b313ef49f27652edb3846b
denishaaddweb <denisha.addweb@gmail.com>
2016-03-01 16:05:57 +0530

    filter code change

--- sites/all/themes/chomchom/js/custom.js
+++ sites/all/themes/chomchom/js/custom.js
@@ -14,12 +14,15 @@
       if(pathArray[2] == 'sang-tiem-search'){
         jQuery("#quicktabs-search ul.quicktabs-tabs li").removeClass("active");
         jQuery("#quicktabs-search ul.quicktabs-tabs li:nth-child(2)").addClass("active");
+        jQuery("#quicktabs-tabpage-search-1").removeClass("quicktabs-hide");
+        jQuery("#quicktabs-tabpage-search-0").addClass("quicktabs-hide");
       }
 
       //disable category field in edit form 
       if (jQuery(".node-type-listing").hasClass("page-node-edit")) {
         jQuery("#edit-field-category select").attr("disabled", true);   
       }
+      
     }
   };
 }(jQuery));
