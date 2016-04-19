654bfdfa6f7d2a65e759c6422ce379db986ab3b7
denishaaddweb <denisha.addweb@gmail.com>
2016-04-19 15:45:42 +0530

    layout change for ads

--- sites/all/themes/chomchom/template.php
+++ sites/all/themes/chomchom/template.php
@@ -6,7 +6,7 @@
 function chomchom_preprocess_page(&$variables) {
 // Add information about the number of sidebars.
  if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second']) || !empty($variables['page']['primary_first']) && !empty($variables['page']['sidebar_second'])) {
-  $variables['content_column_class'] = ' class="col-lg-6 col-md-6 col-sm-4 col-xs-12"';
+  $variables['content_column_class'] = ' class="col-lg-6 col-md-6 col-sm-8 col-xs-12"';
  }
  elseif (!empty($variables['page']['primary_first']) || !empty($variables['page']['sidebar_first']) || !empty($variables['page']['sidebar_second'])) {
   $variables['content_column_class'] = ' class="col-lg-9 col-md-9 col-sm-8 col-xs-12"';
